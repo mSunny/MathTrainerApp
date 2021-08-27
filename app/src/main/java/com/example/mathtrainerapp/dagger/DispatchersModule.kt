81554a762cf90a5f08d04ea30e0cf15e212cd3e9
Alexandra <sunny.volkova@gmail.com>
2021-08-27 09:50:20 +0300

    Added provide methods for all dispatchers

--- app/src/main/java/com/example/mathtrainerapp/dagger/DispatchersModule.kt
+++ app/src/main/java/com/example/mathtrainerapp/dagger/DispatchersModule.kt
@@ -4,11 +4,31 @@ import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
 
 @Module()
 class DispatchersModule {
+    @DefaultDispatcher
     @Provides
-    fun provideDispatcher(): CoroutineDispatcher {
-        return Dispatchers.IO
-    }
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 }
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class DefaultDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class IoDispatcher
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class MainDispatcher
